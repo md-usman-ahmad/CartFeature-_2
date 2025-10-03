3d7d26a2cdb44ff5c621766ab9f50ef79856f415
Usmaan <Usmaan@usmaan.com>
2025-10-04 04:51:29 +0530

    useReducer hook implemented. shifted all cart functions (add/remove) logic into reducer for cleaner state flow

--- src/store/contextApi.js
+++ src/store/contextApi.js
@@ -3,19 +3,99 @@ import { createContext } from "react";
 import { Navbar } from "../component/navbar";
 import { Home } from "../component/home.jsx";
 import { Electronics } from "../component/electronics.jsx";
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import { CartPage } from "../component/cartPage.jsx";
 
-
 export const CartContext = createContext({
-    onAddingAnItemToCart : ()=>{},
-    onRemovingAnItemFromCart : ()=>{},
-    allCartItems: []
-})
+  onAddingAnItemToCart: () => {},
+  onRemovingAnItemFromCart: () => {},
+  allCartItems: [],
+});
 
+export function CartContextProvider() {
+  // const [CartState, setCartState] = useState({
+  //     allCartItems: [],
+  //     currentDisplay: "Home",
+  // });
 
-export function CartContextProvider(){
-    const [CartState, setCartState] = useState({
+  function reducerfn(CartState, action) {
+    if(action.type === "handleCurrentDisplay"){
+      return {
+        ...CartState,
+        currentDisplay: action.payload.CurrentDisplayValue,
+      };
+    } else if (action.type === "handleOnAddingAnItemToCart") {
+      if (CartState.allCartItems.length === 0) {
+          return {
+            ...CartState,
+            allCartItems: [
+              ...CartState.allCartItems,
+              {
+                id : action.payload.id,
+                imgsrc : action.payload.imgsrc,
+                title : action.payload.title,
+                slogan : action.payload.slogan,
+                price : action.payload.price,
+                quantity: 1,
+              },
+            ],
+          };
+      } else {
+        let arr = CartState.allCartItems.filter((item) => {
+          if (item.id === action.payload.id) return true;
+        });
+        console.log("arr = ", arr);
+
+        if (arr.length > 0) {
+            return {
+              ...CartState,
+              allCartItems: CartState.allCartItems.map((item) => {
+                if (item.id === action.payload.id) {
+                  item.quantity = item.quantity + 1;
+                }
+                return item;
+              }),
+            };
+        } else {
+            return {
+              ...CartState,
+              allCartItems: [
+                ...CartState.allCartItems,
+                {
+                  id : action.payload.id,
+                  imgsrc : action.payload.imgsrc,
+                  title : action.payload.title,
+                  slogan : action.payload.slogan,
+                  price : action.payload.price,
+                  quantity: 1,
+                },
+              ],
+            };
+        }
+      }
+    } else if (action.type === "handleOnRemovingAnItemFromCart"){
+      let updatedAllCartItems = [];
+      if (action.payload.quantity > 1) {
+        updatedAllCartItems = CartState.allCartItems.map((item) => {
+          if (item.id === action.payload.id) {
+            item.quantity = item.quantity - 1;
+          }
+          return item;
+        });
+      } else {
+        updatedAllCartItems = CartState.allCartItems.filter((item) => {
+          if (item.id !== action.payload.id) return true;
+        });
+      }
+
+        return {
+          ...CartState,
+          allCartItems: updatedAllCartItems,
+        };
+    }
+  }
+
+  const [CartState, dispatch] = useReducer(reducerfn, {
     allCartItems: [],
     currentDisplay: "Home",
   });
@@ -24,117 +104,56 @@ export function CartContextProvider(){
   let content;
 
   function handleCurrentDisplay(CurrentDisplayValue) {
-    setCartState((prevState) => {
-      return {
-        ...prevState,
-        currentDisplay: CurrentDisplayValue,
-      };
-    });
-  }
-  
-  // Now for each item, the number of times it has been added to the cart (i.e., its quantity) will be shown, and according to that, the price of each item will also be displayed.
-  function handleOnAddingAnItemToCart(id, imgsrc, title, slogan, price) {
-    if (CartState.allCartItems.length === 0) {
-      setCartState((prevState) => {
-        return {
-          ...prevState,
-          allCartItems: [
-            ...prevState.allCartItems,
-            {
-              id,
-              imgsrc,
-              title,
-              slogan,
-              price,
-              quantity: 1,
-            },
-          ],
-        };
-      });
-    } else {
-      let arr = CartState.allCartItems.filter((item) => {
-        if (item.id === id) return true;
-      });
-      console.log("arr = ", arr);
-
-      if (arr.length > 0) {
-        setCartState((prevState) => {
-          return {
-            ...prevState,
-            allCartItems: prevState.allCartItems.map((item) => {
-              if (item.id === id) {
-                item.quantity = item.quantity + 1;
-              }
-              return item;
-            }),
-          };
-        });
-      } else {
-        setCartState((prevState) => {
-          return {
-            ...prevState,
-            allCartItems: [
-              ...prevState.allCartItems,
-              {
-                id,
-                imgsrc,
-                title,
-                slogan,
-                price,
-                quantity: 1,
-              },
-            ],
-          };
-        });
-      }
-    }
-  }
-  function handleOnRemovingAnItemFromCart(id,quantity){
-    console.log("id = ",id, " quantity = ",quantity);
-    let updatedAllCartItems = [];
-    if(quantity > 1){
-      updatedAllCartItems = CartState.allCartItems.map( (item)=>{
-        if(item.id === id){
-          item.quantity = item.quantity - 1
-        }
-        return item;
-      })
-    } else{
-      updatedAllCartItems = CartState.allCartItems.filter( (item)=>{
-        if(item.id !== id) return true;
-      })
-    }
-
-    setCartState( (prevState)=>{
-      return {
-        ...prevState,
-        allCartItems: updatedAllCartItems
+    dispatch({
+      type: "handleCurrentDisplay",
+      payload : {
+        CurrentDisplayValue
       }
     })
   }
 
+  // Now for each item, the number of times it has been added to the cart (i.e., its quantity) will be shown, and according to that, the price of each item will also be displayed.
+  function handleOnAddingAnItemToCart(id, imgsrc, title, slogan, price) {
+    dispatch({
+      type: "handleOnAddingAnItemToCart",
+      payload: {
+        id,
+        imgsrc,
+        title,
+        slogan,
+        price,
+      },
+    });
+  }
+  function handleOnRemovingAnItemFromCart(id, quantity) {
+    console.log("id = ", id, " quantity = ", quantity);
+    dispatch({
+      type: "handleOnRemovingAnItemFromCart",
+      payload : {
+        id,
+        quantity
+      }
+    })
+    
+  }
+
   if (CartState.currentDisplay === "Electronics") {
-    content = (
-      <Electronics
-      ></Electronics>
-    );
+    content = <Electronics></Electronics>;
   } else if (CartState.currentDisplay === "Home") {
     content = <Home></Home>;
   } else if (CartState.currentDisplay === "CartPage") {
-    content = (
-      <CartPage
-        allCartItems={CartState.allCartItems}
-      ></CartPage>
-    );
+    content = <CartPage allCartItems={CartState.allCartItems}></CartPage>;
   }
 
   return (
     <>
-      <CartContext value={{
-        onAddingAnItemToCart : handleOnAddingAnItemToCart,
-        onRemovingAnItemFromCart : handleOnRemovingAnItemFromCart,
-        allCartItems : CartState.allCartItems
-      }}>
+      <CartContext
+        value={{
+          onAddingAnItemToCart: handleOnAddingAnItemToCart,
+          onRemovingAnItemFromCart: handleOnRemovingAnItemFromCart,
+          allCartItems: CartState.allCartItems,
+        }}
+      >
         <Navbar
           currentDisplay={handleCurrentDisplay}
           allCartItems={CartState.allCartItems}
